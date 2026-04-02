705203fa795ee30ff39aa94c619fc86ac13e247f
Xiaofei Cao (from Dev Box) <xiaofeicao@microsoft.com>
2026-04-02 13:11:37 +0800

    add credentialFile to initialization case

--- azure-legacy-sdk-update-azure-client-initialization/src/main/java/com/microsoft/azure/management/clientinitialization/AzureInitialization.java
+++ azure-legacy-sdk-update-azure-client-initialization/src/main/java/com/microsoft/azure/management/clientinitialization/AzureInitialization.java
@@ -1,21 +1,25 @@
 /**
  * https://github.com/Azure/azure-libraries-for-java/blob/b324fabb9ba2c9687614c800c6ae69e189ce990e/azure-mgmt-resources/src/test/java/com/microsoft/azure/management/resources/core/TestBase.java
  */
-package com.microsoft.azure.management;
+package com.microsoft.azure.management.clientinitialization;
 
 import com.microsoft.azure.AzureEnvironment;
 import com.microsoft.azure.AzureResponseBuilder;
 import com.microsoft.azure.credentials.ApplicationTokenCredentials;
+import com.microsoft.azure.management.Azure;
 import com.microsoft.azure.management.resources.core.ResourceGroupTaggingInterceptor;
 import com.microsoft.azure.management.resources.fluentcore.utils.ProviderRegistrationInterceptor;
 import com.microsoft.azure.serializer.AzureJacksonAdapter;
 import com.microsoft.rest.LogLevel;
 import com.microsoft.rest.RestClient;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class AzureInitialization {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // initialize using env
         String clientId = System.getenv("AZURE_CLIENT_ID");
         String tenantId = System.getenv("AZURE_TENANT_ID");
         String clientSecret = System.getenv("AZURE_CLIENT_SECRET");
@@ -40,5 +44,11 @@ public class AzureInitialization {
 
         Azure.Authenticated azureAuthed = Azure.authenticate(builder.build(), subscriptionId, credentials.domain());
         Azure azure = azureAuthed.withSubscription(subscriptionId);
+
+        // initialize using credential file
+        final File credentialFile = new File(System.getenv("AZURE_AUTH_LOCATION"));
+        azure = Azure.configure()
+            .authenticate(credentialFile)
+            .withDefaultSubscription();
     }
 }
